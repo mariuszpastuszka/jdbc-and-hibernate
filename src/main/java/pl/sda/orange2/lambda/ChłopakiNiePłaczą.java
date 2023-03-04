36d70e5ee82a6c7133c778b9f6abc17bd544b393
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-03-04 13:32:28 +0100

    [change] - interface with implementation - polimorthism

---
+++ src/main/java/pl/sda/orange2/lambda/ChłopakiNiePłaczą.java
@@ -0,0 +1,12 @@
+package pl.sda.orange2.lambda;
+
+public class ChłopakiNiePłaczą implements BoysDontCry {
+    @Override
+    public void silnoreki() {
+        System.out.println("Jestem Silnoręki:)");
+    }
+
+    public void sing() {
+        System.out.println("Ruszaj się....");
+    }
+}
--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -3,6 +3,11 @@ package pl.sda.orange2.lambda;
 public class LambdaExercises {
 
     public static void main(String[] args) {
+        BoysDontCry boy = new ChłopakiNiePłaczą();
+        boy.silnoreki();
+        ((ChłopakiNiePłaczą) boy).sing();
 
+        ChłopakiNiePłaczą boy2 = new ChłopakiNiePłaczą();
+        boy2.sing();
     }
 }
